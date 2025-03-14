822b0ababd30c9b4f991e9183e8e6bed641473aa
Ahmed Safayet Amio <ahmedamio620@gmail.com>
2025-03-15 00:26:15 +0600

    Health System Initialized

---
+++ 3knot3/Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 0;
+    private int _currentHealth = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        _currentHealth=_maxHealth;
+    }
+
+    private void Update()
+    {
+        if (_currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDmg(int DmgAmount)
+    {
+        _currentHealth -= DmgAmount;
+    }
+}
