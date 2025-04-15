3718a39407987910d6f3894a75f17c5c3701592c
AuvroIslam <auvro2107024@stud.kuet.ac.bd>
2025-04-16 02:02:24 +0600

    changed MainMenu name

--- Assets/Scripts/UI/MainMenu/MainMenuTransitioner.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuTransitioner.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 using SingletonManagers;
 namespace UI.MainMenu
 {
-    public class MainMenu : MonoBehaviour
+    public class MainMenuTransitioner : MonoBehaviour
     {   
         private void Start()
         {
